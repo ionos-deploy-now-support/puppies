d552d3b1be8c38b7fa1bb5b869e44e1dce4f3476
exagent957 <exagent957@gmail.com>
2023-08-25 21:46:45 -0500

    multer fix

--- controllers/users.js
+++ controllers/users.js
@@ -37,13 +37,13 @@ exports.uploadUserPhoto = upload.single('photo');
 
 exports.resizeUserPhoto = (req, res, next) => {
   if (!req.file) return next();
-  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpg`;
   //sharp is used to resize photos
   sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
-    .toFile(`public/img/users/${req.file.filename}`);
+    .toFile(`client/public/img/users/${req.file.filename}`);
   next();
 };
 
