bddeac80abb5deee758a9f9a1555c7b67a40b1cc
exagent957 <exagent957@gmail.com>
2023-10-19 12:56:15 -0500

    implemented delete communication

--- client/src/pages/CommunicationDelete.jsx
+++ client/src/pages/CommunicationDelete.jsx
@@ -6,12 +6,12 @@ export const action =
   (queryClient) =>
   async ({ params }) => {
     try {
-      await customFetch.delete(`/communications/${params.id}`);
-      queryClient.invalidateQueries(['clients']);
+      await customFetch.delete(`/communications/${params.communicationId}`);
+      queryClient.invalidateQueries(['clientCommunications']);
 
       toast.success('Communication deleted successfully');
     } catch (error) {
       toast.error(error?.response?.data?.message);
     }
-    return redirect('/dashboard/clients');
+    return redirect('../');
   };
