512947d8c4a2ef805eaffdc779f5cb56dee79acb
exagent957 <exagent957@gmail.com>
2023-11-07 18:56:14 -0600

    fixed bug for display of payments

--- client/src/components/PaymentsContainer.jsx
+++ client/src/components/PaymentsContainer.jsx
@@ -3,30 +3,43 @@ import Wrapper from '../assets/wrappers/PaymentsContainer';
 import { usePaymentsContext } from '../pages/PaymentsLayout';
 import { useClientsContext } from '../pages/ClientsLayout';
 import { useContractsContext } from '../pages/ContractsLayout';
+import { useParams } from 'react-router-dom';
 
 const PaymentsContainer = () => {
   const { payments } = usePaymentsContext();
   const { clients } = useClientsContext();
   const { contracts } = useContractsContext();
+  const params = useParams();
+  const contractId = params.id;
   console.log(payments);
   console.log(clients);
-  console.log(contracts);
-  // const client = clients.filter((client) => {
-  //   return client._id === clientId;
-  // });
-  // const clientFirstName = client[0].clientFirstName;
-  if (payments.length === 0) {
+  const contract = contracts.filter((contract) => {
+    return contract._id === contractId;
+  });
+  const contractType = contract[0].contractType;
+  console.log(contract);
+  const clientId = contract[0].client;
+  const client = clients.filter((client) => {
+    return client._id === clientId;
+  });
+  const clientFirstName = client[0].clientFirstName;
+  const contractPayments = payments.filter((payment) => {
+    return payment.contract === contractId;
+  });
+  if (contractPayments.length === 0) {
     return (
       <Wrapper>
-        <h5>No payments entered yet for clientFirstName</h5>
+        <h5>
+          No payments entered yet for {contractType} for {clientFirstName}
+        </h5>
       </Wrapper>
     );
   }
   return (
     <Wrapper>
       <h5>
-        {/* {results} {results > 1 ? 'Payments' : 'Payment record'} */}
-        {` for {clientFirstName}`}
+        {contractPayments.length} {contractPayments.length > 1 ? 'Payments' : 'Payment record'}
+        {` for ${clientFirstName}'s  ${contractType}`}
       </h5>
       <div className="payments">
         {payments.map((payment) => {
