42fd413097bd3fe9337b26cfc9939760327f749d
exagent957 <exagent957@gmail.com>
2023-11-21 19:20:11 -0600

    trying to deploy

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');
-const DB = process.env.MONGODB_URI.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+// const DB = process.env.MONGODB_URI.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.MONGODB_URI;
 
 function connectToMongo() {
   mongoose
